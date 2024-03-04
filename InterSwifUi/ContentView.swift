95fd924b6de4ff1663546a5ff34326a7c35a14b2
Nikita <nik007rev@gmail.com>
2024-03-04 17:12:14 +0300

    Initial Commit

---
+++ InterSwifUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InterSwifUi
+//
+//  Created by Никита  on 04.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InterSwifUi/InterSwifUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterSwifUiApp.swift
+//  InterSwifUi
+//
+//  Created by Никита  on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct InterSwifUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
